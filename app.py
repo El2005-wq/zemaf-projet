6661c721ea71cfb350d79a8b790982c8da3e687f
El2005-wq <zemaf394@gmail.com>
2025-05-26 03:17:17 +0000

    Premier commit - ajout des fichiers zemaf

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bienvenue sur ZEMAF - plateforme académique !"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
